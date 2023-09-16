e7aecf038ed57b7d170792ef62af46df5785523f
rishabh063 <55680979+rishabh063@users.noreply.github.com>
2023-09-16 17:11:01 +0530

    Add files via upload

--- swapper2.py
+++ swapper2.py
@@ -60,21 +60,16 @@ def swap_face(face_swapper,
     target_face = target_faces[target_index]
 
     return face_swapper.get(temp_frame, target_face, source_face, paste_back=True)
- 
-    
+
+
+model = "./inswapper/checkpoints/inswapper_128.onnx" 
+face_analyser = getFaceAnalyser(model)    
+face_swapper = getFaceSwapModel(model)
 def process(source_img: Union[Image.Image, List],
             target_img: Image.Image,
             source_indexes: str,
             target_indexes: str,
             model: str):
-        
-    # load face_analyser
-    face_analyser = getFaceAnalyser(model)
-    
-    # load face_swapper
-    model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model)
-    face_swapper = getFaceSwapModel(model_path)
-    
     # read target image
     target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
     
@@ -86,7 +81,7 @@ def process(source_img: Union[Image.Image, List],
     if target_faces is not None:
         temp_frame = copy.deepcopy(target_img)
         if isinstance(source_img, list) and num_source_images == num_target_faces:
-            print("Replacing faces in target image from the left to the right by order")
+            #print("Replacing faces in target image from the left to the right by order")
             for i in range(num_target_faces):
                 source_faces = get_many_faces(face_analyser, cv2.cvtColor(np.array(source_img[i]), cv2.COLOR_RGB2BGR))
                 source_index = i
@@ -107,24 +102,24 @@ def process(source_img: Union[Image.Image, List],
             # detect source faces that will be replaced into the target image
             source_faces = get_many_faces(face_analyser, cv2.cvtColor(np.array(source_img[0]), cv2.COLOR_RGB2BGR))
             num_source_faces = len(source_faces)
-            print(f"Source faces: {num_source_faces}")
-            print(f"Target faces: {num_target_faces}")
+            #print(f"Source faces: {num_source_faces}")
+            #print(f"Target faces: {num_target_faces}")
 
             if source_faces is None:
                 raise Exception("No source faces found!")
 
             if target_indexes == "-1":
                 if num_source_faces == 1:
-                    print("Replacing all faces in target image with the same face from the source image")
+                    #print("Replacing all faces in target image with the same face from the source image")
                     num_iterations = num_target_faces
                 elif num_source_faces < num_target_faces:
-                    print("There are less faces in the source image than the target image, replacing as many as we can")
+                    #print("There are less faces in the source image than the target image, replacing as many as we can")
                     num_iterations = num_source_faces
                 elif num_target_faces < num_source_faces:
-                    print("There are less faces in the target image than the source image, replacing as many as we can")
+                    #print("There are less faces in the target image than the source image, replacing as many as we can")
                     num_iterations = num_target_faces
                 else:
-                    print("Replacing all faces in the target image with the faces from the source image")
+                    #print("Replacing all faces in the target image with the faces from the source image")
                     num_iterations = num_target_faces
 
                 for i in range(num_iterations):
@@ -140,7 +135,7 @@ def process(source_img: Union[Image.Image, List],
                         temp_frame
                     )
             else:
-                print("Replacing specific face(s) in the target image with specific face(s) from the source image")
+                #print("Replacing specific face(s) in the target image with specific face(s) from the source image")
 
                 if source_indexes == "-1":
                     source_indexes = ','.join(map(lambda x: str(x), range(num_source_faces)))
@@ -187,75 +182,9 @@ def process(source_img: Union[Image.Image, List],
             raise Exception("Unsupported face configuration")
         result = temp_frame
     else:
-        print("No target faces found!")
+        pass
+        #print("No target faces found!")
     
     result_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
     return result_image
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Face swap.")
-    parser.add_argument("--source_img", type=str, required=True, help="The path of source image, it can be multiple images, dir;dir2;dir3.")
-    parser.add_argument("--target_img", type=str, required=True, help="The path of target image.")
-    parser.add_argument("--output_img", type=str, required=False, default="result.png", help="The path and filename of output image.")
-    parser.add_argument("--source_indexes", type=str, required=False, default="-1", help="Comma separated list of the face indexes to use (left to right) in the source image, starting at 0 (-1 uses all faces in the source image")
-    parser.add_argument("--target_indexes", type=str, required=False, default="-1", help="Comma separated list of the face indexes to swap (left to right) in the target image, starting at 0 (-1 swaps all faces in the target image")
-    parser.add_argument("--face_restore", action="store_true", help="The flag for face restoration.")
-    parser.add_argument("--background_enhance", action="store_true", help="The flag for background enhancement.")
-    parser.add_argument("--face_upsample", action="store_true", help="The flag for face upsample.")
-    parser.add_argument("--upscale", type=int, default=1, help="The upscale value, up to 4.")
-    parser.add_argument("--codeformer_fidelity", type=float, default=0.5, help="The codeformer fidelity.")
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == "__main__":
-    
-    args = parse_args()
-    
-    source_img_paths = args.source_img.split(';')
-    print("Source image paths:", source_img_paths)
-    target_img_path = args.target_img
-    
-    source_img = [Image.open(img_path) for img_path in source_img_paths]
-    target_img = Image.open(target_img_path)
-
-    # download from https://huggingface.co/deepinsight/inswapper/tree/main
-    model = "./checkpoints/inswapper_128.onnx"
-    result_image = process(source_img, target_img, args.source_indexes, args.target_indexes, model)
-    
-    if args.face_restore:
-        from restoration import *
-        
-        # make sure the ckpts downloaded successfully
-        check_ckpts()
-        
-        # https://huggingface.co/spaces/sczhou/CodeFormer
-        upsampler = set_realesrgan()
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        codeformer_net = ARCH_REGISTRY.get("CodeFormer")(dim_embd=512,
-                                                         codebook_size=1024,
-                                                         n_head=8,
-                                                         n_layers=9,
-                                                         connect_list=["32", "64", "128", "256"],
-                                                        ).to(device)
-        ckpt_path = "CodeFormer/CodeFormer/weights/CodeFormer/codeformer.pth"
-        checkpoint = torch.load(ckpt_path)["params_ema"]
-        codeformer_net.load_state_dict(checkpoint)
-        codeformer_net.eval()
-        
-        result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR)
-        result_image = face_restoration(result_image, 
-                                        args.background_enhance, 
-                                        args.face_upsample, 
-                                        args.upscale, 
-                                        args.codeformer_fidelity,
-                                        upsampler,
-                                        codeformer_net,
-                                        device)
-        result_image = Image.fromarray(result_image)
-    
-    # save result
-    result_image.save(args.output_img)
-    print(f'Result saved successfully: {args.output_img}')
